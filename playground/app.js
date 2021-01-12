d7aaa35e7db725163df694f68d778d5a4e77ff17
Sameer Al Harbi <smrhrbi@gmail.com>
2021-01-12 22:41:04 +0300

    update

--- playground/app.js
+++ playground/app.js
@@ -1,12 +1,1 @@
 console.log('hi');
-
-const testParams = (req, res) => {
-
-    return function (next) {
-        console.log(next);
-    }
-}
-
-const x = testParams('1a', '2a', '3a');
-
-x('3cc');
